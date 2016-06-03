92398d6a8e8292c5933bc44d0b39bfd1a376af47
Kruy Vanna <kruyvanna@gmail.com>
2016-06-03 22:08:34 +0700

    initial commit

---
+++ app/src/main/java/com/vanna/myapplication/Main2Activity.java
@@ -0,0 +1,21 @@
+package com.vanna.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("NAME");
+
+        TextView textView = (TextView) findViewById(R.id.textView2);
+        textView.setText(name);
+    }
+}
---
+++ app/src/main/java/com/vanna/myapplication/MainActivity.java
@@ -0,0 +1,32 @@
+package com.vanna.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView textView = (TextView) findViewById(R.id.textView);
+                textView.setText("You clicked me");
+
+                Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
+                intent.putExtra("NAME", "KISS");
+                startActivity(intent);
+
+            }
+        });
+    }
+}
